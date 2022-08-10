d22a1ca010fedc988bfc78aef23fced164822810
root <root@alpine.localdomain>
2022-08-10 13:06:23 +0300

    .

--- demo.c
+++ demo.c
@@ -13,5 +13,14 @@ int main(void)
 	fmpz_factor_init(factors);
 	fmpz_init(n);
 
+
+	fmpz_set_str(n, "12387192837918273918723981291837121933111751252512531193171", 10);
+	qsieve_factor(factors, n);
+	fmpz_factor_print(factors);
+	printf("\n");
+
+
+	fmpz_factor_clear(factors);
+	fmpz_clear(n);
 	return 0;
 }

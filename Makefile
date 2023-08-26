a4bc68edf041f8579b2e2edc1b2726c15ecd44d3
Mike <tankf33der@disroot.org>
2023-08-26 10:06:35 +0300

    .

--- Makefile
+++ Makefile
@@ -1,3 +1,7 @@
-all:
+all: qsieve ecm
+
+qsieve: qsieve.c
 	gcc qsieve.c -I /usr/include/flint -lflint -o qsieve.out
 
+ecm: ecm.c
+	gcc ecm.c -I /usr/include/flint -lflint -o ecm.out
---
+++ ecm.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "flint.h"
+#include "fmpz.h"
+#include "fmpz_factor.h"
+#include "qsieve.h"
+
+
+int main(void)
+{
+	fmpz_t factors;
+	fmpz_t n;
+	flint_rand_s state;
+	mp_limb_t B1, B2, curves;
+
+	fmpz_init(factors);
+	fmpz_init(n);
+	flint_randinit(&state);
+
+	fmpz_set_str(n, "123344545454019845440983345568719283791827391872398129183712198719879331117512525125311931711", 10);
+
+	// qsieve_factor(factors, n);
+	fmpz_factor_ecm(factors, curves, B1, B2, &state, n);
+
+	// fmpz_factor_print(factors);
+	printf("\n");
+
+
+	// fmpz_factor_clear(factors);
+	fmpz_clear(n);
+	return 0;
+}
